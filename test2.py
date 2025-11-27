65b89c4176eb03713e57926441df0fe1a9604007
ayusshsoni <ayusshsoni1110@gmail.com>
2025-11-27 13:34:06 +0530

    selenium automation

--- test2.py
+++ test2.py
@@ -1,9 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+"""from selenium import webdriver
+from selenium.webdriver.common.by import By #the locator tool which will help it find buttons and fields in the web page
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome()# launches chrome
 driver.get("https://www.google.com")
 
-search_box = driver.find_element(By.NAME, "q")
-search_box.send_keys("geeksforgeeks")
-search_box.submit()
+search_box = driver.find_element(By.NAME, "q")#finds the search text box
+search_box.send_keys("geeksforgeeks")#types what we want to search in the bar
+search_box.submit()#submits the search like hitting enter 
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By # By used for location HTML elements
+from time import sleep #pauses for 1 second before clicking next page
+
+driver = webdriver.Chrome() #opens chrome
+
+driver.get("https://quotes.toscrape.com/js/") #opens the web site
+
+while True: #an infinite loop, it will only break when there is no 'next' button
+    quotes = driver.find_elements(By.CLASS_NAME, "quote")# 'find_elements() lists all elements with class name quote
+    for q in quotes:
+        print("\n", q.text)#q.text prints it
+
+    try:
+        next_btn = driver.find_element(By.CSS_SELECTOR, "li.next > a") #find the next button
+        next_btn.click()
+        sleep(1)
+    except:
+        print("no more pages")
+        break
